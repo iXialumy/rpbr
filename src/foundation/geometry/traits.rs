32d577f920b95497a7a454bfd2e5c18d77e4d19c
iXialumy <pausjonas@gmail.com>
2020-10-24 21:49:28 +0000

    Move Float trait from vector.rs to traits.rs

--- src/foundation/geometry/traits.rs
+++ src/foundation/geometry/traits.rs
@@ -1,5 +1,60 @@
 use std::fmt::Display;
+use std::cmp::Ordering;
 use std::ops::{Add, Div, Mul, Neg, Sub};
+
+pub trait Float:
+    Add<Output = Self>
+    + Sub<Output = Self>
+    + Mul<Output = Self>
+    + Div<Output = Self>
+    + Neg<Output = Self>
+    + PartialEq
+    + PartialOrd
+    + Copy
+    + Display
+{
+    fn abs(self) -> Self;
+    fn partial_eq(&self, other: &Self) -> bool;
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering>;
+    fn sqrt(self) -> Self;
+    fn is_nan(self) -> bool;
+}
+
+impl Float for f32 {
+    fn abs(self) -> Self {
+        self.abs()
+    }
+    fn partial_eq(&self, other: &Self) -> bool {
+        self == other
+    }
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        std::cmp::PartialOrd::partial_cmp(&self, &other)
+    }
+    fn sqrt(self) -> Self {
+        self.sqrt()
+    }
+    fn is_nan(self) -> bool {
+        self.is_nan()
+    }
+}
+
+impl Float for f64 {
+    fn abs(self) -> Self {
+        self.abs()
+    }
+    fn partial_eq(&self, other: &Self) -> bool {
+        self == other
+    }
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        std::cmp::PartialOrd::partial_cmp(&self, &other)
+    }
+    fn sqrt(self) -> Self {
+        self.sqrt()
+    }
+    fn is_nan(self) -> bool {
+        self.is_nan()
+    }
+}
 
 pub trait Num:
     Add<Output = Self>
--- src/foundation/geometry/vector.rs
+++ src/foundation/geometry/vector.rs
@@ -1,63 +1,9 @@
 use core::fmt;
-use std::cmp::Ordering;
 use std::fmt::{Display, Formatter};
 use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use crate::foundation::geometry::traits::Float;
 
-pub trait Float:
-Add<Output=Self>
-+ Sub<Output=Self>
-+ Mul<Output=Self>
-+ Div<Output=Self>
-+ Neg<Output=Self>
-+ PartialEq
-+ PartialOrd
-+ Copy
-+ Display
-{
-    fn abs(self) -> Self;
-    fn partial_eq(&self, other: &Self) -> bool;
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering>;
-    fn sqrt(self) -> Self;
-    fn is_nan(self) -> bool;
-}
-
-impl Float for f32 {
-    fn abs(self) -> Self {
-        self.abs()
-    }
-    fn partial_eq(&self, other: &Self) -> bool {
-        self == other
-    }
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        std::cmp::PartialOrd::partial_cmp(&self, &other)
-    }
-    fn sqrt(self) -> Self {
-        self.sqrt()
-    }
-    fn is_nan(self) -> bool {
-        self.is_nan()
-    }
-}
-
-impl Float for f64 {
-    fn abs(self) -> Self {
-        self.abs()
-    }
-    fn partial_eq(&self, other: &Self) -> bool {
-        self == other
-    }
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        std::cmp::PartialOrd::partial_cmp(&self, &other)
-    }
-    fn sqrt(self) -> Self {
-        self.sqrt()
-    }
-    fn is_nan(self) -> bool {
-        self.is_nan()
-    }
-}
-
-fn max<T: Float>(first: T, second: T) -> T {
+pub fn max<T: Float>(first: T, second: T) -> T {
     if first < second {
         second
     } else {
@@ -65,7 +11,7 @@ fn max<T: Float>(first: T, second: T) -> T {
     }
 }
 
-fn min<T: Float>(first: T, second: T) -> T {
+pub fn min<T: Float>(first: T, second: T) -> T  {
     if first < second {
         first
     } else {
