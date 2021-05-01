c852907612818832f626791ce347056285bf57da
Jonas Paus <pausjonas@gmail.com>
2021-05-01 21:57:46 +0200

    Add intersection to Shape trait

--- src/foundation/shapes/shape.rs
+++ src/foundation/shapes/shape.rs
@@ -3,7 +3,14 @@ use std::fmt::Debug;
 use num_traits::{AsPrimitive, Float, FromPrimitive};
 
 use crate::foundation::geometry::bounds::Bounds3;
+use crate::foundation::geometry::ray::Ray;
+use crate::foundation::shapes::surface_interaction::SurfaceInteraction;
 use crate::foundation::transform::transform::Transform;
+
+pub struct Intersection<T: Float + FromPrimitive + AsPrimitive<f64> + Copy + Debug> {
+    interaction: SurfaceInteraction<T>,
+    distance: T,
+}
 
 pub trait Shape<T: Float + FromPrimitive + AsPrimitive<f64> + Copy + Debug>: Copy {
     fn new(
@@ -17,4 +24,6 @@ pub trait Shape<T: Float + FromPrimitive + AsPrimitive<f64> + Copy + Debug>: Cop
         (*self.object_to_world())(self.object_bounds())
     }
     fn area(&self) -> T;
+    fn intersect(&self, ray: Ray<T>) -> Option<Intersection<T>>;
+    fn intersect_p(&self, ray: Ray<T>, test_alpha_texture: bool) -> bool;
 }
