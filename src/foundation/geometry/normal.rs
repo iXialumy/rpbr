396171f31af70f85f32e14230a1b8e7cfc5d7b8f
Jonas Paus <pausjonas@gmail.com>
2021-05-07 22:19:04 +0200

    Add From Vector3 Trait to Normal3

--- src/foundation/geometry/normal.rs
+++ src/foundation/geometry/normal.rs
@@ -1,3 +1,4 @@
+use crate::foundation::geometry::vector::Vector3;
 use num_traits::{AsPrimitive, Float, FromPrimitive};
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
@@ -5,6 +6,16 @@ pub struct Normal3<T: Float + FromPrimitive + AsPrimitive<f64> + Copy> {
     pub x: T,
     pub y: T,
     pub z: T,
+}
+
+impl<T: Float + AsPrimitive<f64> + FromPrimitive> From<Vector3<T>> for Normal3<T> {
+    fn from(vector: Vector3<T>) -> Self {
+        Normal3 {
+            x: vector.x,
+            y: vector.y,
+            z: vector.z,
+        }
+    }
 }
 
 impl<T: Float + FromPrimitive + AsPrimitive<f64>> Normal3<T> {
