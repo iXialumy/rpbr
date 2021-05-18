6d8543773ba8bdf643149036545eb09f6197d098
Jonas Paus <pausjonas@gmail.com>
2021-05-18 23:59:55 +0200

    Add Signed Trait to Shape T

--- src/foundation/shapes/shape.rs
+++ src/foundation/shapes/shape.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 
-use num_traits::{AsPrimitive, Float, FromPrimitive};
+use num_traits::{AsPrimitive, Float, FromPrimitive, Signed};
 
 use crate::foundation::geometry::bounds::Bounds3;
 use crate::foundation::geometry::ray::Ray;
@@ -12,7 +12,7 @@ pub struct Intersection<T: Float + FromPrimitive + AsPrimitive<f64> + Copy + Deb
     pub distance: T,
 }
 
-pub trait Shape<T: Float + FromPrimitive + AsPrimitive<f64> + Copy + Debug>: Copy {
+pub trait Shape<T: Float + FromPrimitive + AsPrimitive<f64> + Copy + Debug + Signed>: Copy {
     fn object_to_world(self) -> Transform<T>;
     fn object_bounds(self) -> Bounds3<T>;
     fn world_bounds(self) -> Bounds3<T> {
