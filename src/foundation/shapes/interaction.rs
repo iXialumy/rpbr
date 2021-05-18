17ef22bbfecec957f7aac9fcadf7ce9ad6be1d26
Jonas Paus <pausjonas@gmail.com>
2021-05-19 00:00:14 +0200

    Remove unused import

--- src/foundation/shapes/interaction.rs
+++ src/foundation/shapes/interaction.rs
@@ -3,7 +3,7 @@ use std::fmt::Debug;
 use num_traits::{AsPrimitive, Float, FromPrimitive};
 
 use crate::foundation::geometry::normal::Normal3;
-use crate::foundation::geometry::point::{Point2, Point3};
+use crate::foundation::geometry::point::Point3;
 use crate::foundation::geometry::vector::Vector3;
 
 pub trait Interaction<T: Float + FromPrimitive + AsPrimitive<f64> + Debug> {
